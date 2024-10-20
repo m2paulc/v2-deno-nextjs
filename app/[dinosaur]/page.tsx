50b9f541dcfd3c907b13269359b9ba4364bc4076
Paul Caoile <39387011+m2paulc@users.noreply.github.com>
2024-10-20 16:45:11 -0400

    deno v2 testing with next js

---
+++ app/[dinosaur]/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Dino } from "../type.ts";
+import NextLink from "next/link";
+import React from "react";
+
+type RouteParams = { params: { dinosaur: string } };
+
+const Link = NextLink.default;
+
+export default function Dinosaur(request: RouteParams) {
+	const selectedDinosaur = request.params.dinosaur;
+	const [dinosaur, setDino] = useState<Dino>({ name: "", description: "" });
+
+	useEffect(() => {
+		(async () => {
+			const resp = await fetch(`/api/dinosaurs/${selectedDinosaur}`);
+			const dino = (await resp.json()) as Dino;
+			setDino(dino);
+		})();
+	}, [selectedDinosaur]);
+
+	return (
+		<main className="flex flex-col items-center justify-between m-12 p-4 rounded-lg bg-gray-100 shadow-lg text-black">
+			<h1 className="text-2xl">{dinosaur.name}</h1>
+			<p className="p-4">{dinosaur.description}</p>
+			<Link href="/" className="mb-4">
+				🠠 Back to all dinosaurs
+			</Link>
+		</main>
+	);
+}
